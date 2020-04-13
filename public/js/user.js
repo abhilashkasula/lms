ea0ec4e75ba7ea6af8251d077c9327bf73b955b5
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-13 16:16:36 +0530

    loading books and added search feature in user

---
+++ public/js/user.js
@@ -0,0 +1,43 @@
+const getBooks = function(callback) {
+  fetch('books').then(res => res.json()).then(callback);
+}
+
+const generateBook = function(book) {
+  const bookContainer = document.createElement('div');
+  const name = document.createElement('div');
+  const status = document.createElement('div');
+  name.innerText = book.name;
+  status.innerText = book.isAvailable;
+  status.className = 'book-status';
+  name.className = 'book-name';
+  bookContainer.id = book.id;
+  bookContainer.className = 'book';
+  bookContainer.appendChild(name);
+  bookContainer.appendChild(status);
+  return bookContainer;
+};
+
+const drawBooks = function(books) {
+  console.log(books)
+  const shelf = document.querySelector('#book-shelf');
+  books.forEach(book => shelf.appendChild(generateBook(book)));
+};
+
+const searchBooks = function() {
+  const text = document.querySelector('#search-bar').value;
+  const regex = new RegExp(text, 'i');
+  const books = Array.from(document.querySelectorAll('.book'));
+  books.forEach(book => book.classList.add('hide-display'));
+  books.forEach(book => book.children[0].innerText.match(regex) && book.classList.remove('hide-display'));
+}
+
+const addListeners = function() {
+  const search = document.querySelector('#search-bar');
+  search.oninput = searchBooks;
+}
+
+const main = function() {
+  getBooks(drawBooks);
+  addListeners();
+};
+window.onload = main;
