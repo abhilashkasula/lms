d35c9144b0e494a8b54017fae1174e8b086ab3b5
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-13 15:12:47 +0530

    drawing books in admin's home

---
+++ public/js/admin.js
@@ -0,0 +1,26 @@
+const getBooks = function(callback) {
+  fetch('books').then(res => res.json()).then(callback);
+}
+
+const generateBook = function({id, name, isAvailable}) {
+  const bookContainer = document.createElement('div');
+  bookContainer.id = id;
+  bookContainer.className = 'book';
+  const html = `<div class="book-options"><img class="svg hide delete" onclick="deleteBook(${id})" src="images/delete.svg"></div>
+    <div class="book-name">${name}</div>
+    <div class="book-status">${isAvailable}</div>`
+  bookContainer.innerHTML = html;
+  return bookContainer;
+}
+
+const drawBooks = function(books) {
+  const shelf = document.querySelector('#book-shelf');
+  shelf.innerHTML = '';
+  books.forEach(book => shelf.appendChild(generateBook(book)));
+};
+
+const main = function() {
+  getBooks(drawBooks);
+};
+
+window.onload = main;
